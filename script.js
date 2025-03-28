7c3986503e0070ba6205dcc31e57d45cb8e9edfe
Latrell26 <cruzlatrell5@gmail.com>
2025-03-28 12:49:10 +0800

    march28

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const toggleMenu = document.querySelector(".toggle_menu");
-const headerNav = document.querySelector(".header_nav");
-const backDrop = document.querySelector(".backDrop");
-toggleMenu.addEventListener("click",() => {
-    toggleMenu.classList.toggle("open");
-    headerNav.classList.toggle("open");
-    backDrop.classList.toggle("open");
-} )
+    const toggleMenu = document.querySelector(".toggle_menu");
+    const headerNav = document.querySelector(".header_nav");
+    const backDrop = document.querySelector(".backDrop");
+    toggleMenu.addEventListener("click",() => {
+        toggleMenu.classList.toggle("open");
+        headerNav.classList.toggle("open");
+        backDrop.classList.toggle("open");
+    } )
+
+const headerWrapper = document.querySelector(".header_wrapper");
+window.addEventListener("scroll", ()=> {
+    if (window.scrollY > 100){
+        headerWrapper.classList.add("open");
+    }
+        else{
+            headerWrapper.classList.remove("open")
+        }
+});
